1b5846ed41e214c33221195370be55f4cdf0485a
nathaniel <nrohbock@myriad.com>
2017-08-30 17:42:34 -0600

    Add a directory for excercises to the repository

---
+++ excercises/ex1.py
@@ -0,0 +1,29 @@
+print "Hello World"
+print "Hello again"
+print "I liked typing this"
+print "This is fun."
+print "Yay! Printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+
+# Study drills:
+# 1. Make your script print another line.
+print "\nDrill 1:\n"
+print "Here's another line, but I don't know if this is what you meant"
+
+# 2. Make your script print only one of the lines.
+print "\nDrill 2:\n"
+# print "Hello World"
+# print "Hello again"
+print "I liked typing this"
+# print "This is fun."
+# print "Yay! Printing."
+# print "I'd much rather you 'not'."
+# print 'I "said" do not touch this.'
+
+# Again, not sure if the above is what he meant.
+
+# 3. Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what
+# this character does.
+print "\nDrill 3:\n"
+print "The octothorpe comments out the line, so it won't print."
