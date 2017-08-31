5e386f3cbc98a36fd2f5c55a0470c5437a3f4a1e
nathaniel <nrohbock@myriad.com>
2017-08-30 18:01:03 -0600

    Add and completed exercise 2.

---
+++ excercises/ex2.py
@@ -0,0 +1,9 @@
+# A comment, this is so that you can read your program later.
+# Anything after the # is ignored by python
+
+print "I can have code like this" # and the comment afterward is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "This won't run"
+
+print "This will run."
